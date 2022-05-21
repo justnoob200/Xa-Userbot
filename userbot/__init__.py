94749f21eadf25c9583502f1a9607111b0e1726d
justnoob200 <105341014+justnoob200@users.noreply.github.com>
2022-05-21 17:58:10 +0700

    Update __init__.py

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -172,7 +172,7 @@ if G_BAN_LOGGER_GROUP:
     G_BAN_LOGGER_GROUP = int(G_BAN_LOGGER_GROUP)
 
 # Heroku Credentials for updater.
-HEROKU_MEMEZ = sb(os.environ.get("HEROKU_MEMEZ", "True"))
+HEROKU_MEMEZ = sb(os.environ.get("HEROKU_MEMEZ", "False"))
 HEROKU_APP_NAME = os.environ.get("HEROKU_APP_NAME", "")
 HEROKU_API_KEY = os.environ.get("HEROKU_API_KEY", "")
 
